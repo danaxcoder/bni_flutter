4261909ca8682f9dcecba60cb659b5f58094bad6
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-23 03:54:32 +0800

    Last commit

---
+++ assets/assets/common/js/home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+});
+
+function haveCard() {
+    setTimeout(() => {
+        window.location.href = "card.html";
+    }, 500);
+}
+
+function regNewCard() {
+    setTimeout(() => {
+        window.location.href = "card.html";
+    }, 500);
+}
+
+module.haveCard = haveCard;
+module.regNewCard = regNewCard;
