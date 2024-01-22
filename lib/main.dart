7996923fe57ad43a8a2996b4206c41028b4cec58
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-23 07:09:01 +0800

    Last commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  InAppWebViewController _webViewController;
+  late InAppWebViewController _webViewController;
   String url = "";
   double progress = 0;
 
@@ -44,11 +44,10 @@ class _MyAppState extends State<MyApp> {
                 decoration:
                 BoxDecoration(border: Border.all(color: Colors.blueAccent)),
                 child: InAppWebView(
-                  initialUrl: "https://flutter.dev/",
+                  initialUrlRequest:
+    URLRequest(url: WebUri('https://flutter.dev')),
                   initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                      debuggingEnabled: true,
-                    )
+                    crossPlatform: InAppWebViewOptions()
                   ),
                   onWebViewCreated: (InAppWebViewController controller) {
                     _webViewController = controller;
@@ -70,36 +69,7 @@ class _MyAppState extends State<MyApp> {
                   },
                 ),
               ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    if (_webViewController != null) {
-                      _webViewController.goBack();
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    if (_webViewController != null) {
-                      _webViewController.goForward();
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.refresh),
-                  onPressed: () {
-                    if (_webViewController != null) {
-                      _webViewController.reload();
-                    }
-                  },
-                ),
-              ],
-            ),
+            )
           ]),
         ),
       ),
