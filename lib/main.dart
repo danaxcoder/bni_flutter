6b752ae36c43268a519f512ae6930b5d640913ef
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-23 06:28:19 +0800

    Last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,108 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-InAppLocalhostServer localhostServer = new InAppLocalhostServer();
-
-void main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await localhostServer.start();
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  InAppWebViewController _webViewController;
+  String url = "";
+  double progress = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('InAppWebView Example'),
+        ),
+        body: Container(
+          child: Column(children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "CURRENT URL\n${(url.length > 50) ? url.substring(0, 50) + "..." : url}"
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: progress < 1.0
+                ? LinearProgressIndicator(value: progress)
+                : Container()),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(10.0),
+                decoration:
+                BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: InAppWebView(
+                  initialUrl: "https://flutter.dev/",
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      debuggingEnabled: true,
+                    )
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    _webViewController = controller;
+                  },
+                  onLoadStart: (InAppWebViewController controller, String url) {
+                    setState(() {
+                      this.url = url;
+                    });
+                  },
+                  onLoadStop: (InAppWebViewController controller, String url) async {
+                    setState(() {
+                      this.url = url;
+                    });
+                  },
+                  onProgressChanged: (InAppWebViewController controller, int progress) {
+                    setState(() {
+                      this.progress = progress / 100;
+                    });
+                  },
+                ),
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    if (_webViewController != null) {
+                      _webViewController.goBack();
+                    }
+                  },
+                ),
+                RaisedButton(
+                  child: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    if (_webViewController != null) {
+                      _webViewController.goForward();
+                    }
+                  },
+                ),
+                RaisedButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: () {
+                    if (_webViewController != null) {
+                      _webViewController.reload();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ]),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(
-          url: WebUri('http://localhost:8080/assets/home.html')),
-      )
     );
   }
 }
