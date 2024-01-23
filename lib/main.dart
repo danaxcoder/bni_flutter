5e359e2881c0197f6eb29a325be6a4c80ca90cb9
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-01-23 18:45:33 +0800

    Last commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
     initialSourceType: SourceType.html,
     onWebViewCreated: (controller) { webviewController = controller;
         webviewController.loadContent(
-            'assets/default.html',
+            'assets/home.html',
             SourceType.html,
             fromAssets: true
         );
